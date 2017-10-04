05f78ce4c5d4bb0e35fd551f7a5de891f327f462
Permanent Liu <permanent_liu@foxmail.com>
2017-10-04 15:43:19 +0800

    1

--- 2017.10.04/PostRepositoryByMap.java
+++ 2017.10.04/PostRepositoryByMap.java
@@ -1,38 +1,44 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-public class PostRepositoryByMap implements PostRepository {
+public class PostRepositoryByMap implements PostRepository 
+{
 
     // your code here：你可以在这里进行必要的初始化
-    private Map<Long, Post> postMap;
+    private Map<Long, Post> postMap = new HashMap<Long, Post>();
 
     @Override
-    public void add(Post post) {
+    public void add(Post post) 
+    {
         // your code here
     	postMap.put(post.getId(), post);
     }
 
     @Override
-    public Post getPostById(long id) {
+    public Post getPostById(long id) 
+    {
         // your code here
     	return postMap.get(id);
     }
 
     @Override
-    public void remove(long id) {
+    public void remove(long id) 
+    {
         // your code here
     	postMap.remove(id);
     }
 
     @Override
-    public List<Post> getAll() {
-        // your code here
+    public List<Post> getAll() 
+    {
     	List<Post> posts = new ArrayList<>();
-    	for (int i = 0; i < postMap.size(); i++)
+    	
+    	for (Post p : postMap.values())
     	{
-    		posts.addAll(postMap.values());
-    		
+    		posts.add(p);
     	}
         return posts;
     }
