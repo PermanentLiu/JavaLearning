afa93beb4f6b9db741548f380d33b85443feefd7
Permanent Liu <permanent_liu@foxmail.com>
2017-10-05 22:11:49 +0800

    1

---
+++ 2017.10.05/LYH2/ConsoleInOut.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleInOut
+{
+	public static void main(String[] args) throws IOException
+	{
+		InputStreamReader inputStreamReader = null;
+		
+		try
+		{
+			inputStreamReader = new InputStreamReader(System.in);
+			System.out.println("Enter characters, 'q' to quit");
+			
+			char c;
+			
+			do
+			{
+				c = (char) inputStreamReader.read();
+				System.out.print(c);
+			} 
+			while (c != 'q');
+		}
+		finally
+		{
+			if (inputStreamReader != null)
+			{
+				inputStreamReader.close();
+			}
+		}
+	}
+}
