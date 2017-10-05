b129b18d8ff4b0fba64da4728a335d4127821042
Permanent Liu <permanent_liu@foxmail.com>
2017-10-05 19:48:36 +0800

    1

---
+++ 2017.10.05/CopyFileByte.java
@@ -0,0 +1,39 @@
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyFileByte
+{
+	public static void main(String[] args) throws IOException
+	{
+		FileInputStream inputStream = null;
+		FileOutputStream outputStream = null;
+		
+		try
+		{
+			inputStream = new FileInputStream("C:\\myData\\formalFiles\\codes\\myCodes\\JAVA\\Text50\\src\\input.txt");
+			outputStream = new FileOutputStream("C:\\myData\\formalFiles\\codes\\myCodes\\JAVA\\Text50\\src\\output.txt");
+			
+			int c;
+			
+			while ((c = inputStream.read(null)) != -1)
+			{
+				outputStream.write(c);
+			}
+		}
+		finally
+		{
+			if (inputStream != null)
+			{
+				inputStream.close();
+			}
+			
+			if (outputStream != null)
+			{
+				outputStream.close();
+				
+			}
+		}
+	}
+}
