9c0b0ed7a1e14c3c46143ed8c8c99f31e32eda9e
Permanent Liu <permanent_liu@foxmail.com>
2017-10-25 21:09:33 +0800

    1

---
+++ 2017.10.25/HelloWorld/app/src/main/java/com/permanent_liufoxmail/helloworld/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.permanent_liufoxmail.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 10142 on 2017/10/24.
+ */
+
+public class SecondActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
