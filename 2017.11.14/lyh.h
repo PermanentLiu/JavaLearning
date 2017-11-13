42f515829849386079673a4167b5f3db4c8c5f4e
Permanent Liu <permanent_liu@foxmail.com>
2017-11-14 01:08:07 +0800

    1

---
+++ 2017.11.14/lyh.h
@@ -0,0 +1,26 @@
+#ifndef _LYH_H_
+#define _LYH_H_
+
+
+int cheng_er(int x);
+int jia_yi(int x);
+
+int cheng_er(int x)
+{
+	int y;
+	
+	y = x * 2;
+	
+	return (y);
+}
+
+int jia_yi(int x)
+{
+	int y;
+	
+	y = x + 1;
+	
+	return (y);
+}
+
+#endif 
