109af71d88f593b42c3459fcd1b535b3c0bd7bd7
Permanent Liu <permanent_liu@foxmail.com>
2018-02-18 00:29:51 +0800

    1

---
+++ 2018.02.18/app/src/main/java/com/permanent_liufoxmail/pl/frontground/MainActivity.java
@@ -0,0 +1,17 @@
+package com.permanent_liufoxmail.pl.frontground;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.permanent_liufoxmail.pl.R;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
