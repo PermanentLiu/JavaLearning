4453a442d8384b4a50b85ea2c2f7654e222a1ec5
Permanent Liu <permanent_liu@foxmail.com>
2018-03-30 23:04:17 +0800

    1

--- 2018.03.30/Text1.java
+++ 2018.03.30/Text1.java
@@ -5,50 +5,28 @@ import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Text11
 {
 	public static void main(String[] args)
 	{
-		String url = "http://www.baidu.com/";
-		String rule = "<a .*href=.+</a>";
+		String url = "http://www.permanentliu.cn/";
 		
 		try
 		{
 			URL tempURL = new URL(url);
 			URLConnection connection = tempURL.openConnection();
 			InputStream inputStream = connection.getInputStream();
+			
 			InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
+			
 			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 			
-			Pattern pattern = Pattern.compile(rule);
-			Matcher matcher = null;
-			
-			Set<String> set = new HashSet<>();
-			String tempContent = "";
 			
 			String content = "";
 			while ((content = bufferedReader.readLine()) != null)
 			{
-//				System.out.println(content);
-				
-				matcher = pattern.matcher(content);
-				
-				while (matcher.find())
-				{
-					tempContent = matcher.group();
-					set.add(tempContent);
-				}
-				
-			}
-			
-			for (String t : set)
-			{
-				System.out.println(t);
+				System.out.println(content);
 			}
 			
 			
