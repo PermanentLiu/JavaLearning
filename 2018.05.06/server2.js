10ac5c4de1cac725162f15a83ecfe109aed86cea
Permanent Liu <permanent_liu@foxmail.com>
2018-05-26 22:32:58 +0800

    1

--- 2018.05.06/server2.js
+++ 2018.05.06/server2.js
@@ -88,14 +88,11 @@ http.createServer(function(req, res){
 	res.write("\n");
 	res.write("score: " + params.score); 
     res.end();
-
+	
 	var modSql = 'UPDATE user SET score = ? WHERE name = ?';
 	var modSqlParams = [params.score, params.name];
-	
-	
 	//改
 	connection.query(modSql,modSqlParams,function (err, result) {
-		
 	   if(err){
 			 console.log('[UPDATE ERROR] - ',err.message);
 			 return;
@@ -103,73 +100,9 @@ http.createServer(function(req, res){
 	  console.log('--------------------------UPDATE----------------------------');
 	  console.log('UPDATE affectedRows',result.affectedRows);
 	  console.log('-----------------------------------------------------------------\n\n');
-	  return;
 	});
   
 }).listen(11113);
 console.log("start modify!");
-
-http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-  
-	 // 解析 url 参数
-    var params = url.parse(req.url, true).query;
-    res.write("name：" + params.name);
-    res.end();
-	
-	var delSql = 'DELETE FROM user WHERE name = ?';
-	var delSqlParams = [params.name];
-	//改
-	connection.query(delSql,delSqlParams,function (err, result) {
-	   if(err){
-			 console.log('[DELETE ERROR] - ',err.message);
-			 return;
-	   }        
-	  console.log('--------------------------DELETE----------------------------');
-	  console.log('DELETE affectedRows',result.affectedRows);
-	  console.log('-----------------------------------------------------------------\n\n');
-	});
-  
-}).listen(11114);
-
-console.log("start delete!");
-
-http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	
-	// 解析 url 参数
-    var params = url.parse(req.url, true).query;
-    //res.write("name：" + params.name);
-	
-	var  sql = 'SELECT * FROM user';
-	//查
- 
-	
-	connection.query(sql,function (err, result) {
-		if(err){
-			console.log('[SELECT ERROR] - ',err.message);
-			return;
-		}
-		//console.log(result);
-		
-		////////////////////////////////////////////////////
-		var myobj = eval(result);
-		
-		for (var i = 0; i < myobj.length; i++){
-			if (myobj[i].name == params.name){
-				console.log("score:" + myobj[i].score);
-				console.log("   NO."+ i);
-				res.end([myobj[i].score, i]);
-			}
-		}
-		
-
-	});
-	
-	
-	
-	
-}).listen(11110);
-console.log("start search0");
 
 
